d3950436159e19ac279a12937753a725be817c79
bpgibson1 <84873880+bpgibson1@users.noreply.github.com>
2021-10-21 17:24:23 -0500

    Initial commit

---
+++ src/main/java/dmacc/DogOwnersApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogOwnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogOwnersApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dmacc/beans/Owner.java
@@ -0,0 +1,13 @@
+/**
+ * @author Bryner Gibson - bpgibson1
+ * CIS175 - Fall 2021
+ * Oct 21, 2021
+ */
+package dmacc.beans;
+
+public class Owner {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String contactNumber;
+}
