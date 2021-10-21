e77aada58eb436913eae827b63ba143e2fe0789b
bpgibson1 <84873880+bpgibson1@users.noreply.github.com>
2021-10-21 17:25:28 -0500

    Initial Commit

--- src/main/java/dmacc/beans/Owner.java
+++ src/main/java/dmacc/beans/Owner.java
@@ -10,4 +10,60 @@ public class Owner {
 	private String firstName;
 	private String lastName;
 	private String contactNumber;
+	
+	public Owner() {
+		super();
+	}
+
+	public Owner(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Owner(String firstName, String lastName, String contactNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.contactNumber = contactNumber;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Owner [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", contactNumber="
+				+ contactNumber + "]";
+	}
+	
 }
